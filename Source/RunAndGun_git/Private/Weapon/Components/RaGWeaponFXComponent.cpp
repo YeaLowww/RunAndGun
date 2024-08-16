d8a41a5f1e8f018cce8df8c26d48f719201de574
YeaLow <dsrnjhszz@gmail.com>
2024-08-16 15:16:14 +0300

    Commit - Decals

--- Source/RunAndGun_git/Private/Weapon/Components/RaGWeaponFXComponent.cpp
+++ Source/RunAndGun_git/Private/Weapon/Components/RaGWeaponFXComponent.cpp
@@ -4,34 +4,41 @@
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/DecalComponent.h"
 
 URaGWeaponFXComponent::URaGWeaponFXComponent()
 {
-	PrimaryComponentTick.bCanEverTick = false;
-
+    PrimaryComponentTick.bCanEverTick = false;
 }
-
 
 void URaGWeaponFXComponent::BeginPlay()
 {
-	Super::BeginPlay();
-
-	
+    Super::BeginPlay();
 }
 
-void URaGWeaponFXComponent::PlayImpactFX(const FHitResult& Hit) {
-    auto Effect = DefaultEffect;
+void URaGWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
+{
+    auto ImpactData = DefaultImpactData;
 
-	if (Hit.PhysMaterial.IsValid())
+    if (Hit.PhysMaterial.IsValid())
     {
         const auto PhysMat = Hit.PhysMaterial.Get();
-        if (EffectsMap.Contains(PhysMat))
+        if (ImpactDataMap.Contains(PhysMat))
         {
-            Effect = EffectsMap[PhysMat];
+            ImpactData = ImpactDataMap[PhysMat];
         }
-	}
-    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+    }
+    // niagara
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactData.NiagaraEffect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
 
+    // decal
+    auto DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(),  //
+        ImpactData.DecalData.Material,                  //
+        ImpactData.DecalData.Size, Hit.ImpactPoint,     //
+        Hit.ImpactNormal.Rotation());                   //
+    if (DecalComponent)
+    {
+        DecalComponent->SetFadeOut(ImpactData.DecalData.LifeTime, ImpactData.DecalData.FadeOutTime);
+    }
 }
-
-
--- Source/RunAndGun_git/Public/Weapon/Components/RaGWeaponFXComponent.h
+++ Source/RunAndGun_git/Public/Weapon/Components/RaGWeaponFXComponent.h
@@ -3,6 +3,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "RaGCoreTypes.h"
 #include "RaGWeaponFXComponent.generated.h"
 
 class UNiagaraSystem;
@@ -22,8 +23,8 @@ protected:
     
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-    UNiagaraSystem* DefaultEffect;
+    FImpactData DefaultImpactData;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-    TMap<UPhysicalMaterial*, UNiagaraSystem*> EffectsMap;
+    TMap<UPhysicalMaterial*, FImpactData> ImpactDataMap;
 };
